8d2c59db930b5cf0f1111220972109f340a66494
Gungun <gungun@Gunguns-MacBook-Pro.local>
2019-03-09 18:47:29 +0530

    first commit

--- src/app/modeldrivenc/modeldrivenc.component.ts
+++ src/app/modeldrivenc/modeldrivenc.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl,FormGroup} from '@angular/forms'
+import {FormControl,FormGroup,Validator} from '@angular/forms'
 @Component({
   selector: 'app-modeldrivenc',
   templateUrl: './modeldrivenc.component.html',
   styleUrls: ['./modeldrivenc.component.css']
 })
 export class ModeldrivencComponent implements OnInit {
-  userForm= new FormGroup({name :new FormControl(),email :new FormControl(),
-    address:new FormGroup({street:new FormControl(),postelcode:new FormControl(),city:new FormControl()})})
+  // userForm= new FormGroup({name :new FormControl(),email :new FormControl(),
+  //   address:new FormGroup({street:new FormControl(),postelcode:new FormControl(),city:new FormControl()})})
+  
+  registerForm=new FormGroup({name:new FormControl("abhishek",),email:new FormControl(),
+    address:new FormGroup({street:new FormControl(),city:new FormControl(),postelcode:new FormControl("2345")})})
+  
   constructor() { }
 
   ngOnInit() {
@@ -15,6 +19,6 @@ export class ModeldrivencComponent implements OnInit {
   onSubmit()
   {
 
-    console.log(this.userForm.value)
+    console.log(this.registerForm.value)
   }
 }
