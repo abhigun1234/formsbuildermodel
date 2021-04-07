d1625d5a8e8669492bb464a377d46ee4b15a4f26
Abhishek <abhi.ghosh27feb@gmail.com>
2021-04-07 21:32:00 +0530

    add

--- src/app/modeldriven/modeldriven.component.ts
+++ src/app/modeldriven/modeldriven.component.ts
@@ -7,21 +7,20 @@ import {FormGroup,FormControl,Validators} from '@angular/forms'
 })
 export class ModeldrivenComponent implements OnInit {
   flag=true;
-  userForm=new FormGroup({name :new FormControl('manish',[Validators.required,Validators.minLength(3),Validators.maxLength(7)]),
-  email:new FormControl(''),address:new FormGroup({street:new FormControl(),
-    city:new FormControl(),state:new FormControl(),postelcode:new FormControl()})})
-  // userForm =new  FormGroup({name:new FormControl('abhishek',[Validators.required])})
-  // userForm=new FormGroup({name:new FormControl(),email:new FormControl(),address:new FormGroup({
+  // userForm=new FormGroup({name :new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(7)]),
+  // email:new FormControl(''),age:new FormControl('',Validators.required),
+  // address:new FormGroup({street:new FormControl(),
+  //   city:new FormControl(),state:new FormControl(),postelcode:new FormControl()})})
+  userForm=new FormGroup({name:new FormControl('abhishek',[Validators.required,Validators.minLength(6)])})
 
-  //   street:new FormControl(),city :new FormControl(),postelcode:new FormControl()
-  // })})
+
   constructor() { }
 
   ngOnInit() {
   }
   onSubmit()
   {
-    debugger
+
     console.log(this.userForm.value)
     
   }
